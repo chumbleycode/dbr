b7161cf256525cd91893f3a5321d4a330d8d7e8b
Justin Chumbley <justin.chumbley@pm.me>
2019-02-21 15:32:34 +0100

    bookkeeping, update readme, attempt to get autodownload for bioconductor pkgs in DESCRIPTION

--- R/utility_tfbm.R
+++ R/utility_tfbm.R
@@ -30,15 +30,20 @@ load_tfbm_matrix =
 
 #' Title
 #'
-#' @param x blah
+#' @param x the output of limma::eBays
+#' @param of_in # string refering to a single collumn of design matrix by name
+#' @param ... any other argument to toptable
 #'
 #' @return  blah
 #' @export
 #' @import tidyverse
 #'
 #' @examples
-tidy_topTable = function(x) {
-  x %>% as_tibble(rownames = "gene")
+tidy_topTable = function(x, of_in, ...) {
+
+  x %>%
+    topTable(coef = of_in, n = Inf, ...) %>%
+    as_tibble(rownames = "gene")
 }
 
 ########################################################
@@ -420,6 +425,18 @@ regress =
 extract_db =
   function(x, methods = NULL){
     # x is returned from infer_db()
+
+    ########################################################
+    # THE FUNCTION "extracter" GIVES A TABLE OF P-VALUES FOR DIFFERENT METHODS:
+    # p_uni  = SIMPLE UNIVARIATE REGRESSION OF B ON EACH TFBM
+    # p_cov  = MULTPLE REGRESSION OF B ON ALL TFBM's SIMULTANIOUSLY
+    # p_tot  = SIMPLE UNIVARIATE OF B ON THE TOTAL NUMBER SITES (NOT SPECIFIC TO ONE TFBM)
+    # p_par  = parametric telis, RETURNED  ONLY IF ARGUMENT 'tT_sub' IS GIVEN TO "db"
+    # p_npar = nonparametric telis, RETURNED  ONLY IF ARGUMENT 'tT_sub' IS GIVEN TO "db"
+    ########################################################
+
+# NOTE: p_tot IS JUST ONE REGRESSION (OF DIFFERENTIAL EFFECT), THE RESULT IS
+# REPLICATED FOR CONVENIENCE
 
     # print("CHECK THE 2 * below!!!!!")
 
